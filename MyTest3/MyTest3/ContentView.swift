1d760731a84e7708e5a422384abf1192449aafdb
Johnson Liu <johnson.liu@nesswell.com>
2022-11-15 20:27:32 -0500

    2022-11-15, add in progress view, and dismiss it after all data are loaded.

--- MyTest3/MyTest3/ContentView.swift
+++ MyTest3/MyTest3/ContentView.swift
@@ -7,30 +7,51 @@
 
 import SwiftUI
 
+extension View {
+    @ViewBuilder func isHidden(_ isHidden: Bool) -> some View {
+        if isHidden {
+            self.hidden()
+        } else {
+            self
+        }
+    }
+}
+
 struct ContentView: View {
     @State var books = [Book]()
     
+    @State var isHidden = false
+    
     var body: some View {
         
-        List(books, id: \.id) { item in
-            VStack(alignment: .leading) {
-                Text(item.id)
-                    .font(.headline)
-                Text(item.title)
-                HStack(alignment: .top) {
-                    Text(item.author.last_name)
-                    Text(", ")
-                    Text(item.author.first_name)
-                }
-                HStack(alignment: .top) {
-                    Text(item.author.location.city)
-                    Text(", ")
-                    Text(item.author.location.nation)
+        ZStack {
+            List(books, id: \.id) { item in
+                VStack(alignment: .leading) {
+                    Text(item.id)
+                        .font(.headline)
+                    Text(item.title)
+                    HStack(alignment: .top) {
+                        Text(item.author.last_name)
+                        Text(", ")
+                        Text(item.author.first_name)
+                    }
+                    HStack(alignment: .top) {
+                        Text(item.author.location.city)
+                        Text(", ")
+                        Text(item.author.location.nation)
+                    }
                 }
             }
-        }
-        .task {
-            await loadData()
+            .task {
+                await loadData()
+            }
+            
+            VStack {
+                ProgressView("Loading...")
+                    .tint(.blue)
+                    .isHidden(isHidden)
+            }
+            
         }
     }
     
@@ -44,6 +65,8 @@ struct ContentView: View {
             let (data, _) = try await URLSession.shared.data(from: url)
             if let decoderResponse = try? JSONDecoder().decode([Book].self, from: data) {
                 books = decoderResponse
+                
+                isHidden = true
             }
         } catch {
             print("Invalid Data.")
